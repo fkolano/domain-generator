b214427d53e4a4b26a414155e0274ab95ea1a928
fkolano <fkolano@gmail.com>
2022-05-21 00:38:08 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function domainGenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let p = 0; p < adj.length; p++) {
+          console.log(`${pronoun[i]}${noun[j]}${adj[p]}.com`);
+        }
+      }
+    }
+  }
+  domainGenerator();
 };
